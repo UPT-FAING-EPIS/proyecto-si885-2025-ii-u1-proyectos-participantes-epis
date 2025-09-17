8017b12009e8006e13de1051634650f95e7e694b
CarlosAyala1989 <ca2022074266@virtual.upt.pe>
2025-09-17 00:47:05 -0500

    Modificacion a las bases de datos para que sean mas rentables

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,31 +14,33 @@ provider "google" {
   region  = var.gcp_region
 }
 
-// Recurso: Instancia de Cloud SQL para MySQL (sin cambios)
+// Recurso: Instancia de Cloud SQL para MySQL
 resource "google_sql_database_instance" "mysql_instance" {
   name             = "mi-instancia-mysql-demo"
   database_version = "MYSQL_8_0"
   region           = var.gcp_region
 
   settings {
-    tier = "db-n1-standard-1"
+    tier = "db-f1-micro"
+    
+    # --- CONFIGURACIÓN DE AHORRO MÁXIMO ---
+    # La política 'NEVER' le dice a la instancia que se detenga
+    # tras unos minutos de inactividad. Solo se activará (y facturará)
+    # cuando reciba una conexión.
+    activation_policy = "NEVER"
   }
   
   deletion_protection = false
 }
 
-// MODIFICADO: Este bloque ahora creará una base de datos por cada nombre en la variable "database_names".
+// Bloque para crear bases de datos dinámicamente
 resource "google_sql_database" "databases" {
-  # for_each necesita un conjunto (set) de strings, así que convertimos la lista.
   for_each = toset(var.database_names)
-
   instance = google_sql_database_instance.mysql_instance.name
-  
-  # "each.key" contendrá el nombre de la base de datos en cada iteración.
   name     = each.key
 }
 
-// MODIFICADO: El usuario ahora toma su nombre de una variable.
+// Bloque para crear el usuario de la base de datos
 resource "google_sql_user" "db_user" {
   instance = google_sql_database_instance.mysql_instance.name
   name     = var.db_user_name
